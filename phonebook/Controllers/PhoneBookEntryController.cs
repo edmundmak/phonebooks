c2f7eefc16883ac5db1fb4f55d0fe600a5ecc924
edmundm <edmund.makuwaza@iwyze.co.za>
2021-01-11 18:10:41 +0200

    fix project structure

---
+++ phonebook/Controllers/PhoneBookEntryController.cs
@@ -0,0 +1,42 @@
+﻿using common.infrastructure.Requests;
+using common.infrastructure.Responses;
+using dataaccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phonebook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhoneBookEntryController : ControllerBase
+    {
+        private readonly ILogger<PhoneBookEntryController> _logger;
+        private IDataService _services;
+        public PhoneBookEntryController(ILogger<PhoneBookEntryController> logger, IDataService services )
+        {
+            _logger = logger;
+            _services = services;
+        }
+
+        [HttpPost]
+        public PhoneBookResponses GetPhoneBookEntries([FromBody] GetPhoneBookRequest getPhoneBookRequest)
+        {
+            var response = _services.GetPhoneEntryByPhoneBookName(getPhoneBookRequest);
+            return response;
+        }
+
+        [HttpPost("createentry")]
+        public CreatePhoneBookResponses CreatePhoneBooks([FromBody] CreatePhoneBookRequest createPhoneBookRequest)
+        {
+            var response = _services.CreatePhoneBooks(createPhoneBookRequest);
+            return response;
+
+        }
+
+    }
+}
---
+++ phonebook/Startup.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace phonebook
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+
+            //services.AddControllers().AddNewtonsoftJson();
+            //services.AddMvc().AddJsonOptions(o =>
+            //{
+            //    o.JsonSerializerOptions.PropertyNamingPolicy = null;
+            //    o.JsonSerializerOptions.DictionaryKeyPolicy = null;
+            //});
+
+
+
+            //services.AddMvc(options =>
+            //{
+            //    options.UseCustomStringModelBinder();
+            //    options.AllowEmptyInputInBodyModelBinding = true;
+            //    foreach (var formatter in options.InputFormatters)
+            //    {
+            //        if (formatter.GetType() == typeof(SystemTextJsonInputFormatter))
+            //            ((SystemTextJsonInputFormatter)formatter).SupportedMediaTypes.Add(
+            //                Microsoft.Net.Http.Headers.MediaTypeHeaderValue.Parse("text/plain"));
+            //    }
+            //}).AddJsonOptions(options =>
+            //{
+            //    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+            //});
+
+            
+
+            services.AddTransient<dataaccess.IDataService, dataaccess.DataService>();
+
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("AllowMyOrigin", builder =>
+            //         builder.WithOrigins("*").AllowAnyHeader()
+            //                            .AllowAnyMethod());
+            //});
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.WithOrigins("http://localhost:4200", "http://localhost:44349")
+                    .AllowAnyMethod().AllowAnyHeader());
+                //.AllowCredentials());
+            });
+            services.AddControllers();
+
+
+
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+            //app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
