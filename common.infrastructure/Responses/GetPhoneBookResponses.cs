8bd228b4c6f5fef819cc41f056c99e81e8ebe695
edmundm <edmund.makuwaza@iwyze.co.za>
2021-02-21 18:16:18 +0200

    response updated

---
+++ common.infrastructure/Responses/GetPhoneBookResponses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace common.infrastructure.Responses
+{
+    public class GetPhoneBookResponses
+    {
+        public List<BookResponses> Books { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+    }
+}
